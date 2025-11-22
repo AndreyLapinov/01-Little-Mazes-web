8a9b84335d3b4fa13ccb2330c1983c95c4a87df1
AndreyLapinov <aalapinov@gmail.com>
2025-11-22 16:13:10 +0300

    251122_export_1613

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1763816430|1953233924';
+const CACHE_VERSION = '1763817154|2677002730';
 /** @type {string} */
 const CACHE_PREFIX = '01_Little_Mazes-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
